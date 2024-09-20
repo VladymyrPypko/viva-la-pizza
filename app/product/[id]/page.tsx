da904993e31f0b5809708570548b61608f053cb9
VladymyrPypko <zaitm99@gmail.com>
2024-09-20 14:55:30 +0300

    product-image component

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,9 +1,18 @@
-export default function ProductPage({
+import { Container, ProductImage } from "@/components/common";
+import { prisma } from "@/prisma/prisma-client";
+import { notFound } from "next/navigation";
+
+export default async function ProductPage({
   params: { id },
 }: {
   params: { id: string };
 }) {
+  const product = await prisma.product.findFirst({ where: { id: Number(id) }})
+  if (!product) return notFound();
+
   return (
-    <p>product {id}</p>
+    <Container className="flex flex-col my-10">
+      <ProductImage imageUrl={product.imageUrl} name={product.name} size={20}/>
+    </Container>
   );
 }
